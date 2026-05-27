49fa01cb102d2378f391bff204e70b87ff923932
León <leonvargas4991@gmail.com>
2026-05-27 13:34:32 -0300

    Delete launch_tapitocam.sh

--- launch_tapitocam.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Wrapper to launch tapitocam.sh from a desktop entry
-# This uses $HOME, which is correctly expanded by desktop environments
-cd "$HOME/Downloads/TapitoCAM" || exit
-./tapitocam.sh
